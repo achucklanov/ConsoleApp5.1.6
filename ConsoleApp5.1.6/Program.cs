22b7ddc178a6c41ea7470897cfd653fb5e4e4362
AndreyChuck <a.chucklanov@yandex.ru>
2023-03-17 22:26:07 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp5.1.6/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp5._1._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetArrayFromConsole();
+
+            int temp;
+
+            
+            for (int i = 0; i < result.Length; i++)
+            {
+                for (int j = i + 1; j < result.Length; j++)
+                {
+                    if (result[i] > result[j])
+                    {
+                        temp = result[i];
+                        result[i] = result[j];
+                        result[j] = temp;
+                    }
+                }
+            }
+
+            foreach (var item in result)
+            {
+                Console.Write(item);
+            }
+
+        }
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            return result;
+        }
+        
+
+    }
+}
